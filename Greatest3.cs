3ba24e02ec9734a1df6d37a81b51a302517d12d5
Sayali241196 <sayalikhairnar24@gmail.com>
2022-03-02 17:05:07 +0530

    Add project files.

---
+++ Greatest3.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Greatest3
+    {
+      public  static void Main (string[] args)
+            {
+               
+                int first, second, third;
+                Console.WriteLine("Enter the first number : ");
+                first = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the second number : ");
+                second = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Enter the third number : ");
+                third = int.Parse(Console.ReadLine());
+
+                if (first > second)
+                {
+              
+                    if (first > third)
+                    {
+                        Console.WriteLine("largest number : " + first);
+                    }
+                    else
+                    {
+                        Console.WriteLine("largest number : " + third);
+                    }
+                }
+                else
+                {
+                    if (second > third)
+                    {
+                        Console.WriteLine("largest number : " + second);
+                    }
+                    else
+                    {
+                        Console.WriteLine("largest number : " + third);
+                    }
+                }
+            }
+        }
+    }
---
+++ MethodOverloading.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class MethodOverloading
+    {
+        public void add(int a, int b)
+            {
+                Console.WriteLine(a + b);
+            }
+            public void add(float x, float y)
+            {
+                Console.WriteLine(x + y);
+            }
+            public void add(string s1, string s2)
+            {
+                Console.WriteLine(s1 + s2);
+            }
+            static void Main(string[] args)
+            {
+            MethodOverloading obj = new MethodOverloading();
+                obj.add(10, 20);
+                obj.add(10.5f, 20.5f);
+                obj.add("sayali", "khairnar");
+               Console.ReadKey();
+            }
+        }
+    }
