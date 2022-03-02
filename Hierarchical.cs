883bca15afaa770642b6090ec297c7b65d14b921
Sayali241196 <sayalikhairnar24@gmail.com>
2022-03-02 19:07:58 +0530

    updated program

---
+++ Hierarchical.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment
+{
+    class Hierarchical
+    {
+        public class Parent
+        {
+            public void DisplayParentsAB()
+            {
+                Console.WriteLine("A and B are my parents");
+            }
+        }
+
+        public class ChildC : Parent
+        {
+            public void DisplayChildC()
+            {
+                Console.WriteLine("I am the child C");
+            }
+        }
+
+        public class ChildD : Parent
+        {
+            public void DisplayChildD()
+            {
+                Console.WriteLine("I am the child D");
+            }
+        }
+
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+                ChildC cc = new ChildC();
+                ChildD cd = new ChildD();
+
+                cc.DisplayChildC();
+                cc.DisplayParentsAB();
+
+                cd.DisplayChildD();
+                cd.DisplayParentsAB();   
+            }
+        }
+    }
+}
+
